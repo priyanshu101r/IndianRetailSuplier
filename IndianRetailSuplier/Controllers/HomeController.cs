4cc84b83a23fcc1d131fb0018c14c1849658a6a7
prinashu101r <priyanshu101r@gmail.com>
2022-01-06 13:57:03 +0530

    home controller

--- IndianRetailSuplier/Controllers/HomeController.cs
+++ IndianRetailSuplier/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace IndianRetailSuplier.Controllers
 
         public IActionResult Index()
         {
-            _logger.LogInformation("First something");
+            _logger.LogInformation("First something nitin");
             return View();
         }
 
