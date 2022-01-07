f84054b762b6856170c05150c3963b48d5d775fe
priyanshu code <priyanshu101r@gmail.com>
2022-01-07 15:23:24 +0530

    added the bundling

---
+++ IndianRetailSuplier/Common/ExtensionMethods/EnumerableHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndianRetailSuplier.Common.ExtensionMethods
+{
+    public static class EnumerableHelper
+    {
+        public static bool HasValue(this string str)
+        {
+            return !string.IsNullOrWhiteSpace(str);
+        }
+        //public static bool HasItems<T>(this IEnumerable<T> enumerable)
+        //{
+        //    return !enumerable.IsEnumerableNullOrEmpty();
+        //}
+    }
+}
